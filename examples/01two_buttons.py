e81558b3c7d22bf0cf056b70c066f69f4383b1b4
Edgar-Donk <45669035+Edgar-Donk@users.noreply.github.com>
2018-12-08 12:28:24 +0100

    Create 01two_buttons.py

---
+++ examples/01two_buttons.py
@@ -0,0 +1,12 @@
+# comparing the button widgets
+from tkinter.ttk import Style
+from tkinter import Tk
+from tkinter import Button as orig
+from tkinter.ttk import Button as tile
+
+root=Tk()
+s = Style()
+s.theme_use('default')
+origbutton(root,text='original').pack()
+tilebutton(root,text='ttk themed').pack()
+tilebutton(root,text='2nd ttk').pack()
