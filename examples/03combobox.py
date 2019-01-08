83c850e8f589906552f9224128d26b33ac4397d7
Edgar-Donk <45669035+Edgar-Donk@users.noreply.github.com>
2019-01-08 13:00:46 +0100

    Create 03combobox.py

---
+++ examples/03combobox.py
@@ -0,0 +1,20 @@
+from tkinter.ttk import Style, Combobox
+from tkinter import Tk
+root = Tk()
+style = Style()
+style.theme_use('default') # this is needed even though we are using 'default' in theme_settings
+style.theme_settings("default", {
+   "TCombobox": {          # widget class name
+       "configure": {"padding": 5}, # note how we use style.configure and style.map
+       "map": {
+           "background": [("active", "green2"), # background refers to downarrow
+                          ("!disabled", "green4")],
+               # on first selecting from list the text background is grey, inherited from default theme
+               "fieldbackground": [("!disabled", "green3")], # fieldbackground refers to field
+               "foreground": [("focus", "OliveDrab1"),
+                          ("!disabled", "OliveDrab2")]
+           }
+   }
+})
+combo = Combobox(values=['apple', 'banana', 'orange']).pack()
+root.mainloop()
