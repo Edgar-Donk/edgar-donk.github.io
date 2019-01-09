8075f973ff7261ec769889d46e2b884f28946517
Edgar-Donk <45669035+Edgar-Donk@users.noreply.github.com>
2019-01-09 20:28:08 +0100

    Create 06checkbox_themes.py

---
+++ examples/06checkbox_themes.py
@@ -0,0 +1,48 @@
+from pprint import pformat
+from tkinter import Tk, Message
+from tkinter.ttk import Style, Combobox, Button, Checkbutton
+
+def theme_changed(theme):
+    style.theme_use(theme)
+
+    lay = style.layout(tWidg)
+    
+    ppout = pformat(lay)
+
+    style.configure(
+        'Custom.' + tWidg,
+        background='#FFFFFF', # White
+        bordercolor='#00FF00', # Green
+        lightcolor='#FF0000', # Red
+        darkcolor='#0000FF', # Blue
+        borderwidth=4,
+        foreground='#00FFFF', # Cyan
+        fieldbackground='#FF00FF', # Magenta
+        troughcolor='#FFFF00', # Yellow
+        arrowcolor='#A52A2A', # brown
+        focuscolor='#40E0D0' # turquoise
+    )
+    return ppout 
+
+root = Tk()
+style = Style()
+tWidg = 'TCheckbutton'
+combo = Combobox(root, values=sorted(style.theme_names()), state='readonly')
+combo.set(style.theme_use())
+combo.bind('<<ComboboxSelected>>', lambda _e: theme_changed(combo.get()))
+combo.pack()
+
+out = theme_changed(style.theme_use())
+
+button = Checkbutton(root, text="Normal Button")
+button.pack()
+
+button = Checkbutton(root, style="Custom." + tWidg, text="Custom Button")
+button.pack()
+
+mess = Message(root, text='layout \n\n'+out, width=250)
+mess.pack()
+
+root.mainloop()
+
+
