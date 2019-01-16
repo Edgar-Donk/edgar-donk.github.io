b364dbc65db5dfddf29534f779e18cbcb7fc4886
Edgar-Donk <45669035+Edgar-Donk@users.noreply.github.com>
2019-01-16 12:55:51 +0100

    Update 02labelframe.py

--- examples/02labelframe.py
+++ examples/02labelframe.py
@@ -1,10 +1,15 @@
+'''
+LabelFrame is split, and we have to have two separate configure clauses to be able to alter
+the colours of the Label (anciliary part) and the Frame (main part). 2 LabelFrames are 
+displayed one with a label colour change  the other with a frame colour change.
+'''
 from tkinter import Frame,Tk
 from tkinter.ttk import LabelFrame,Style
 
 root = Tk()
 
 s = Style()
-ch = 'lightgreen'   # change colour here
+ch = 'lightgreen'   # change label colour here, we could program different colours to suit the application requirements
 s.configure(ch +'.TLabelframe.Label', background=ch)  # altering label - anciliary part
 s.configure('pink.TLabelframe', background='pink')    # altering main part
 
