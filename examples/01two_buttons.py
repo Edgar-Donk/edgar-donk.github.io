dc9c77256e6e487d38f0e189fe4518dacffde18b
Edgar-Donk <45669035+Edgar-Donk@users.noreply.github.com>
2019-01-16 12:17:48 +0100

    Update 01two_buttons.py

--- examples/01two_buttons.py
+++ examples/01two_buttons.py
@@ -1,12 +1,12 @@
 # comparing the button widgets
 from tkinter.ttk import Style
 from tkinter import Tk
-from tkinter import Button as orig
-from tkinter.ttk import Button as tile
+from tkinter import Button as origbutton
+from tkinter.ttk import Button as tilebutton
 
 root=Tk()
 s = Style()
-s.theme_use('default')
+s.theme_use('default') # using the ttk default scheme so that all os will be similar 
 origbutton(root,text='original').pack()
 tilebutton(root,text='ttk themed').pack()
 tilebutton(root,text='2nd ttk').pack()
